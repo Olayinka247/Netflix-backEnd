acbcb8665dc53bcc4101fcf5a5846c754c89d029
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-05-20 15:07:53 +0100

    deploying

--- src/server.js
+++ src/server.js
@@ -10,8 +10,26 @@ import {
 
 const server = express();
 
-const port = 3002;
+const port = process.env.PORT || 3002;
 
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+
+const corsOptions = {
+  origin: (origin, next) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      next(null, true);
+    } else {
+      next(
+        createError(
+          400,
+          `Cors Error! your origin ${origin} is not in the list!`
+        )
+      );
+    }
+  },
+};
+
+server.use(cors(corsOptions));
 server.use(express.json());
 
 server.use("/media", mediaRouter);
